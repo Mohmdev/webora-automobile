9357bf80b6bd469e3ce340b2cd017d24a5b8cd67
Mohmdev <mohammadr_m@outlook.com>
2025-03-21 13:26:54 +0300

    fix(build-error): wrapped AdminSearch in a Suspense component to handle loading states.

--- src/components/layouts/admin-header.tsx
+++ src/components/layouts/admin-header.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react"
 import { AdminSearch } from "../admin/search"
 
 export const AdminHeader = async () => {
@@ -5,7 +6,13 @@ export const AdminHeader = async () => {
     <header className="flex h-[60px] items-center gap-4 px-6">
       <div className="grid w-full flex-1 grid-cols-3 items-center gap-4 md:gap-8">
         <div className="col-span-1">
-          <AdminSearch />
+          <Suspense
+            fallback={
+              <div className="h-9 w-full animate-pulse rounded-md bg-primary-800" />
+            }
+          >
+            <AdminSearch />
+          </Suspense>
         </div>
       </div>
     </header>
